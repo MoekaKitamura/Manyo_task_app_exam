c4b75be8e098b68482616d95696fa37e78263c34
moeka kitamura <moeka.k76@gmail.com>
2022-10-18 09:59:04 +0900

    add colums to tasks table

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,10 @@
 class Task < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
+  validates :deadline_on, presence: true
+  validates :priority, presence: true
+  validates :status, presence: true
+
+  enum priority: { low: 0, middle: 1, high: 2 }
+  enum status: { todo: 0, doing: 1, done: 2 }
 end

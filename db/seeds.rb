7aaa532c1afb984b00b2880579d09367a8c5ab85
moeka kitamura <moeka.k76@gmail.com>
2022-10-18 22:18:11 +0900

    edit seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,36 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-50.times do |i|
-  Task.find_or_create_by(
+Task.create!(
+  title: 'first_task',
+  content: 'ごはんを炊く',
+  deadline_on: Date.today,
+  priority: 'high',
+  status: 'done',
+)
+
+Task.create!(
+  title: 'second_task',
+  content: '梅干しを買う',
+  deadline_on: Date.today.since(1.day),
+  priority: 'middle',
+  status: 'doing',
+)
+
+Task.create!(
+  title: 'third_task',
+  content: 'おにぎりを作る',
+  deadline_on: Date.today.since(2.day),
+  priority: 'low',
+  status: 'todo',
+)
+
+7.times do |i|
+  Task.find_or_create_by!(
     title: "Task#{i+1}",
-    content: "腹筋#{i*10}回",
+    content: "腹筋#{i*100}回",
+    deadline_on: Date.today.since(3.day),
+    priority: 'low',
+    status: 'todo',
   )
 end
